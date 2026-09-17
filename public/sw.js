78a4d21a8fe08e342e51eb6aed023d40105ff0c4
ryangdonald-art <ryan.g.donald@gmail.com>
2026-09-18 00:17:28 +0200

    feat: add minimal RALPH PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const CACHE = 'ralph-shell-v1';
+const SHELL = ['/', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  const requestUrl = new URL(event.request.url);
+  if (event.request.method !== 'GET' || requestUrl.origin !== self.location.origin || requestUrl.pathname.startsWith('/api/')) return;
+  event.respondWith(fetch(event.request).then((response) => {
+    if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/'))));
+});
